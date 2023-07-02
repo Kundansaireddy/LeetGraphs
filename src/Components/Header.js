215ea3630d85c7fd53c1163fcd63c3ca4cea9736
Kundansaireddy <81676583+Kundansaireddy@users.noreply.github.com>
2023-07-02 23:20:07 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
       <span className="leetmate">
         &nbsp;&nbsp;&nbsp;
         {/* <img src={Img} alt="logo" width="30" height="30" /> */}
-        &nbsp; LEET GRAPHS
+        &nbsp; LeetGraphs
       </span>
       <p className="problemCount">
         &nbsp;&nbsp;&nbsp; User Count: {props.numberOfProblems}
